e875fc8101c69589922b7badeca8d7fb119ff7bc
Victoria <emerroa@gmail.com>
2026-02-12 12:41:20 +0100

    deleted comment

--- models/mart/mart_faa_stats.sql
+++ models/mart/mart_faa_stats.sql
@@ -1,16 +1,3 @@
-/*SELECT * FROM prep_weather_daily;
-
-SELECT COUNT(diverted::BOOL), SUM(diverted) FROM prep_flights; -- Count boolean returns all rows because the 0s are read as numbers,
--- that's why we need to use SUM instead
-
-SELECT count(DISTINCT dest) AS nunique_to
-FROM prep_flights
-GROUP BY origin;
-*/
-
-
--- CTE
-
 WITH departures AS (
 			SELECT origin,
 				   COUNT(DISTINCT origin) AS nunique_to,
@@ -54,19 +41,3 @@ FROM total_stats ts
 JOIN {{ref('prep_airports')}} ap
 ON ts.airport_code = ap.faa
 
-
-/*
-SELECT dest,
-	   a.city,
-	   a.country,
-	   a.name,
-	   COUNT(DISTINCT origin) AS nunique_from,
-	   COUNT(sched_arr_time) AS arr_planned,
-	   SUM(cancelled) AS arr_cancelled,
-	   SUM(diverted) AS arr_diverted,
-	   COUNT(*) - SUM(cancelled) AS arr_n_flights_calc,
-	   COUNT(arr_time) AS arr_n_flights
-FROM prep_flights f
-JOIN prep_airports a
-ON dest = faa
-GROUP BY f.dest,  a.city, a.country, a.name;
