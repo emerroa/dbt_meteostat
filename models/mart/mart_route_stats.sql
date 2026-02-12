c86c7b180704e6d45d85e8fd31c831208262039c
Victoria <emerroa@gmail.com>
2026-02-12 15:46:10 +0100

    Completed route mart model

---
+++ models/mart/mart_route_stats.sql
@@ -0,0 +1,47 @@
+WITH durations as (
+			SELECT origin,
+				   dest,
+				   MAKE_INTERVAL(mins => actual_elapsed_time::INT) AS f_actual_elapsed_time,
+				   MAKE_INTERVAL(mins => arr_delay::INT) AS f_arr_delay
+			FROM {{ref('prep_flights')}}
+			GROUP BY origin, dest, f_actual_elapsed_time, f_arr_delay
+),
+aggregations AS (
+			SELECT origin,
+				   dest,
+				   AVG(f_actual_elapsed_time) AS avg_f_elapsed_time,
+				   AVG(f_arr_delay) AS avg_arr_delay,
+				   MAX(f_arr_delay) AS max_delay,
+				   MIN(f_arr_delay) AS min_delay
+			FROM durations 
+			GROUP BY origin, dest
+)
+SELECT f.origin AS origin_airport,
+	   ai1.city,
+	   ai1.country,
+	   ai1.name,
+	   f.dest AS dest_airport,
+	   ai2.city,
+	   ai2.country,
+	   ai2.name,
+	   COUNT(*) AS total_flights_p_route,
+	   COUNT(DISTINCT tail_number) AS unique_airplanes,
+	   COUNT(DISTINCT airline) AS unique_airlines,
+	   avg_f_elapsed_time,
+	   avg_arr_delay,
+	   max_delay,
+	   min_delay,
+	   SUM(cancelled) AS total_n_cancelled,
+	   SUM(diverted) AS total_n_diverted
+FROM aggregations ag, {{ref('prep_flights')}} f
+JOIN {{ref('prep_airports')}} ai1
+ON f.origin = ai1.faa
+JOIN {{ref('prep_airports')}} ai2
+ON f.dest = ai2.faa
+GROUP BY f.origin, f.dest, 
+		 ai1.city, ai1.country, ai1.name, 
+		 ai2.city, ai2.country, ai2.name, 
+		 avg_f_elapsed_time, 
+		 avg_arr_delay,
+		 max_delay, 
+		 min_delay
