e0f1d7056a9aa39c0c6d6469c1855456024ac4cc
Victoria <emerroa@gmail.com>
2026-02-13 15:16:44 +0100

    Tried fixing an issue from a mart file

--- models/mart/mart_selected_faa_stats_weather.sql
+++ models/mart/mart_selected_faa_stats_weather.sql
@@ -1,36 +1,3 @@
--- Departures
-SELECT origin,
-	   dest, 
-	   COUNT (DISTINCT dest) AS u_dep_connections,
-	   COUNT(*) AS total_f_planned,
-	   SUM(cancelled) AS total_f_cancelled,
-	   SUM(diverted) AS total_f_diverted,
-	   (COUNT (*) - SUM(cancelled)) - SUM(diverted) AS total_actual_dep
-FROM flights f
-JOIN airports ai1 ON f.origin = ai1.faa
-JOIN airports ai2 ON f.dest = ai2.faa
-WHERE origin IN ('JFK', 'LAX', 'MIA')
-GROUP BY origin, dest
-
--- Arrivals
-SELECT dest,
-	   origin, 
-	   COUNT (DISTINCT origin) AS u_arr_connections,
-	   COUNT(*) AS total_f_planned,
-	   SUM(cancelled) AS total_cancelled,
-	   SUM(diverted) AS total_f_diverted,
-	   (COUNT(*) - SUM(cancelled)) - SUM(diverted) AS total_actual_arr
-FROM flights f
-JOIN airports ai1 ON f.origin = ai1.faa
-JOIN airports ai2 ON f.dest = ai2.faa
-WHERE dest IN ('JFK', 'LAX', 'MIA')
-GROUP BY dest, origin
-
-
-SELECT * FROM flights
-
--- CTE
-
 WITH departures AS (
 				SELECT origin,
 					   dest, 
@@ -39,7 +6,7 @@ WITH departures AS (
 					   SUM(cancelled) AS total_f_cancelled,
 					   SUM(diverted) AS total_f_diverted,
 					   (COUNT (*) - SUM(cancelled)) - SUM(diverted) AS total_actual_dep
-				FROM flights f
+				FROM {{ref('prep_flights')}}
 				WHERE origin IN ('JFK', 'LAX', 'MIA')
 				GROUP BY origin, dest
 ),
@@ -51,7 +18,7 @@ arrivals AS (
 					   SUM(cancelled) AS total_cancelled,
 					   SUM(diverted) AS total_f_diverted,
 					   (COUNT(*) - SUM(cancelled)) - SUM(diverted) AS total_actual_arr
-				FROM flights f
+				FROM {{ref('prep_flights')}}
 				WHERE origin IN ('JFK', 'LAX', 'MIA')
 				GROUP BY dest, origin
 ),
@@ -70,7 +37,7 @@ SELECT ai1.city AS origin_city,
 	   ai2.name AS dest_airport_name,
 	   SUM(u_arr_connections) AS total_unique_arr_con, 
 	   avg_u_arr_con
-FROM flights f
+FROM {{ref('prep_flights')}} f
 CROSS JOIN aggregations
 JOIN departures d ON f.origin = d.origin AND f.dest = d.dest
 JOIN arrivals a ON f.origin = a.origin AND f.dest = a.dest
@@ -81,24 +48,3 @@ GROUP BY origin_city, origin_country, origin_airport_name,
 		 avg_u_dep_con, avg_u_arr_con
 
 
-
-/*
- * 
-- only the airports we collected the weather data for
-- unique number of departures connections
-- unique number of arrival connections
-- how many flight were planned in total (departures & arrivals)
-- how many flights were canceled in total (departures & arrivals)
-- how many flights were diverted in total (departures & arrivals)
-- how many flights actually occured in total (departures & arrivals)
-- *(optional) how many unique airplanes travelled on average*
-- *(optional) how many unique airlines were in service  on average* 
-- (optional) add city, country and name of the airport
-- daily min temperature
-- daily max temperature
-- daily precipitation 
-- daily snow fall
-- daily average wind direction 
-- daily average wind speed
-- daily wnd peakgust
- */
